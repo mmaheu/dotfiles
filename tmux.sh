7ef297b7716f2021eeb282e206475cd608c97142
Mike Maheu <mike@codemartians.com>
2025-05-23 17:50:01 -0400

    redo

--- tmux.sh
+++ tmux.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-session="Alien"
+# Start in my home directory
+cd ~/
 
-tmux new-session -d -s $session
+# Works with tmux 2.6 on Ubuntu 18.04. Your mileage may vary.
+# 'send-keys' exists only to show/prove what's going where: the space prevents command history retention
+#   and the hashtag indicates a comment so bash doesn't try to run it.
 
-window=0
-tmux new-window -t $session:$window -n 'VIM'
+session="alien"
 
-window=1
-tmux new-window -t $session:$window -n 'TERM'
+# Removed: seems unnecessary.
+# tmux start-server
 
-window=2
-tmux new-window -t $session:$window -n 'NOTES'
+# Create new session, and extra windows.
+tmux new-session -d -s $session -n VIM
 
-window=3
-tmux new-window -t $session:$window -n 'MUSIC'
+# A second whole window!
+tmux new-window -t $session:2 -n TERM
+tmux split-window -h -p 50
+tmux select-pane -t 1
 
+# A THIRD whole window!
+tmux new-window -t $session:3 -n NOTES
+
+# A FORTH whole window!
+tmux new-window -t $session:4 -n MUSIC
+
+# Select the second window, then the first window (so that the second window is the 'next' one).
+tmux select-window -t $session:2
+tmux select-window -t $session:1
+
+# Make it so. This must be the very last command.
 tmux attach-session -t $session
